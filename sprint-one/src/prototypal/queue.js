da268da29438fdd1d3b95d73675b172ac1d33f6a
Derek Olson <derekjosepholson@gmail.com>
2015-07-02 17:43:39 -0500

    done

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  instance.count = 0;
+  instance.properties = 0;
+  instance.lowestIndex = 0;
+  return instance;
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+	enqueue: function(value){
+		this[this.count] = value;
+		this.count++;
+		this.properties++;
+	},
+	dequeue: function(){
+		if (this.properties > 0){
+			this.properties--;
+			this.lowestIndex++;
+			return this[this.lowestIndex - 1];
+		}
+	},
+	size: function(){
+		return this.properties;
+	}
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+  instance.count = 0;
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	push: function(value){
+	  this[this.count+ ''] = value;
+	  this.count ++;	
+	},
+	pop: function(){
+      if(this.count > 0){
+      	this.count--;
+      	return this[this.count + ''];
+      }
+	},
+	size: function(){
+		return this.count;
+	}
+};
 
 
